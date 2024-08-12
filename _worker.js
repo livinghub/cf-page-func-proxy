85c00620bb276926b36367add908aac7aef1b759
livinghub <707884308@qq.com>
2024-08-12 15:48:52 +0800

    Add protocol

--- _worker.js
+++ _worker.js
@@ -2,6 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
+        url.protocol = "https:";
         url.hostname=(env.HOSTNAME || "qq707.duckdns.org");
         url.port=(env.PORT || "12225");
         let new_request=new Request(url,request);
@@ -11,3 +12,4 @@ export default {
       return env.ASSETS.fetch(request);
     }
   };
+
