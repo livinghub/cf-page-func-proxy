b656fdb761832839885617841d81a2b7463489d8
livinghub <707884308@qq.com>
2024-08-12 11:55:51 +0800

    Add env port

--- _worker.js
+++ _worker.js
@@ -2,7 +2,8 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname=(env.HOSTNAME || "example.com");
+        url.hostname=(env.HOSTNAME || "bing.com");
+        url.port=(env.PORT || "443");
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
